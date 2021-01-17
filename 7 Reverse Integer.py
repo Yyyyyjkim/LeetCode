8e05a28485f34a87b24ca28eb08243a5eb7b82c2
Yujung Kim <dbwjd159753@hanmail.net>
2021-01-17 19:01:47 +0900

    2021.01.17

---
+++ 7 Reverse Integer.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def reverse(self, x):
+        if x>=0:
+            a = list(str(x))
+            a.reverse()
+            result = int("".join(a))
+        else:
+            a = list(str(x))[1:]
+            a.reverse()
+            result = int("".join(a))*(-1)
+
+        if result < (-2**31) or result >= (2**31):
+            return 0
+        else:
+            return result
+        
+x = -123
+x = 123
+x = 120
+x = 0
